db078420a085619fc68cf3498692767eef2de978
Nick Larson <nlarson@email.arizona.edu>
2015-12-29 11:53:53 -0800

    Finished toggle background and toggle trackers tests

--- app/src/androidTest/java/com/targetprops/greenscreen/GreenScreenEspressoTest.java
+++ app/src/androidTest/java/com/targetprops/greenscreen/GreenScreenEspressoTest.java
@@ -3,14 +3,15 @@ package com.targetprops.greenscreen;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
+import android.support.v4.content.ContextCompat;
 import android.test.ActivityInstrumentationTestCase2;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.doubleClick;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 /**
@@ -31,14 +32,48 @@ public class GreenScreenEspressoTest extends ActivityInstrumentationTestCase2<Co
         mActivity = getActivity();
     }
 
-    public void testTogglePenColor() {
-        onData(withId(R.id.action_colors)).perform(click());
-        onView(withId(R.id.background_blue)).perform(click());
+    public void testTogglePenColorBlue() {
+        onView(withId(R.id.action_colors)).check(matches(isDisplayed()));
+        onView(withId(R.id.action_colors)).perform(click());
+        onView(withText("Blue")).perform(click());
 
         Drawable background = mActivity.findViewById(android.R.id.content).getBackground();
         int backgroundColor = ((ColorDrawable) background).getColor();
 
-        assertEquals(backgroundColor, R.color.blue);
+        assertEquals(backgroundColor, ContextCompat.getColor(mActivity, R.color.blue));
     }
 
+    public void testTrackerLocationCorners() {
+        onView(withId(R.id.action_trackers)).perform(click());
+
+
+        assertTrue(mActivity.imageViewLT.isShown());
+        assertTrue(mActivity.imageViewRT.isShown());
+        assertTrue(mActivity.imageViewLB.isShown());
+        assertTrue(mActivity.imageViewRB.isShown());
+        assertFalse(mActivity.imageViewC.isShown());
+    }
+
+    public void testTrackerLocationAll() {
+        onView(withId(R.id.action_trackers)).perform(click());
+        onView(withId(R.id.action_trackers)).perform(click());
+
+        assertTrue(mActivity.imageViewLT.isShown());
+        assertTrue(mActivity.imageViewRT.isShown());
+        assertTrue(mActivity.imageViewLB.isShown());
+        assertTrue(mActivity.imageViewRB.isShown());
+        assertTrue(mActivity.imageViewC.isShown());
+    }
+
+    public void testTrackerLocationNone() {
+        onView(withId(R.id.action_trackers)).perform(click());
+        onView(withId(R.id.action_trackers)).perform(click());
+        onView(withId(R.id.action_trackers)).perform(click());
+
+        assertFalse(mActivity.imageViewLT.isShown());
+        assertFalse(mActivity.imageViewRT.isShown());
+        assertFalse(mActivity.imageViewLB.isShown());
+        assertFalse(mActivity.imageViewRB.isShown());
+        assertFalse(mActivity.imageViewC.isShown());
+    }
 }
